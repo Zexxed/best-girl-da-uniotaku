035f116698f2fcc5ac0f181a1b7ade6f11a8a899
qgustavor <qgustavor@users.noreply.github.com>
2022-07-21 00:30:11 -0300

    Import current project
    Just Vite template with some few changes

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <a href="https://tracker.uniotaku.com/forums.php?action=viewtopic&topicid=1185">
+      <img src="./assets/logo.svg" class="logo" alt="Logo" />
+    </a>
+    <div class="coming-soon">Em breve!</div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+}
+.coming-soon {
+  font-style: italic;
+}
+</style>
